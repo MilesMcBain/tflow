ff2ea0489cdbd1b0d1890c30cfaadf18699514eb
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2021-11-26 14:06:09 +1000

    new addins: load from any store, make current plan

--- R/addins.R
+++ R/addins.R
@@ -35,17 +35,19 @@ rs_load_current_editor_targets <- function() {
 tflow_load_all <- function() {
   message("\nLoading `packages.R` and `R/*.R`")
   if (file.exists("packages.R")) {
-    suppressPackageStartupMessages(source('packages.R'))
+    suppressPackageStartupMessages(source("packages.R"))
   } else {
     message("No `packages.R` found")
   }
-  if(dir.exists("R") && length(list.files("R", pattern = "\\.[Rr]$"))) {
+  if (dir.exists("R") && length(list.files("R", pattern = "\\.[Rr]$"))) {
     lapply(list.files("R", pattern = "\\.[Rr]$", full.names = TRUE), function(f) {
-      tryCatch(source(f, verbose = FALSE),
-               error = function(e) {
-                 e$message <- paste0("Error in ", f, ": ", e$message)
-                 message(e)
-               })
+      tryCatch(
+        source(f, verbose = FALSE),
+        error = function(e) {
+          e$message <- paste0("Error in ", f, ": ", e$message)
+          message(e)
+        }
+      )
     })
   } else {
     message("No R source files found in R/ directory")
@@ -54,11 +56,71 @@ tflow_load_all <- function() {
 }
 
 #' @noRd
-#' 
+#'
 #' @export
 rs_make_target_at_cursor <- function() {
   word_or_selection <- atcursor::get_word_or_selection()
   command <- bquote(targets::tar_make(.(as.symbol(word_or_selection)), shortcut = TRUE))
-  cat(format(command), "\n", sep = "")
+  cat_commnd(command)
   eval(command)
 }
+
+#' @noRd
+#' @export
+rs_tar_make_current_plan <- function() {
+
+  if (!file.exists("_targets.yaml")) {
+    cat_command(quote(targets::tar_make()))
+    targets::tar_make()
+    return()
+  }
+
+  yaml_file <- parse_targets_yaml()
+  current_file <- fs::path_file(rstudioapi::getActiveDocumentContext()$path)
+
+  yaml_entry <-
+    yaml_file[yaml_file$script == current_file, ]
+
+  if (nrow(yaml_entry) == 0) stop("{tflow} could't find an entry for current active source file in _targets.yaml")
+  if (nrow(yaml_entry) > 1) stop("{tflow} found more than one entry in _targets.yaml matching the current active source file")
+
+  make_command <- bquote(targets::tar_make(script = .(yaml_entry$script), store = .(yaml_entry$store)))
+  cat_command(make_command)
+  eval(make_command)
+}
+
+rs_load_target_at_cursor_from_any_plan <- function() {
+  if (!file.exists("_targets.yaml")) {
+    return(targets:::tar_rstudio_addin_load())
+  }
+  selected_target <- atcursor::get_word_or_selection()
+  yaml_file <- parse_targets_yaml()
+  eval_env <- parent.frame()
+  for (row in seq(nrow(yaml_file))) {
+    yaml_entry <- yaml_file[row, ]
+    current_meta <- tryCatch(
+      targets::tar_meta(store = yaml_entry$store),
+      # there may be no meta for this plan
+      error = function(e) NULL
+    )
+    # if there was no meta continue search
+    if (is.null(current_meta)) next
+
+    if (any(current_meta$name == selected_target)) {
+      found_store_entry <- TRUE
+      load_command <- bquote(targets::tar_load(.(as.symbol(selected_target)), store = .(yaml_entry$store)))
+      cat_command(load_command)
+      return(eval(load_command, envir = eval_env))
+    }
+  }
+  # if we got here we didn't find any matching targets in any stores
+  stop("{tflow} couldn't find ", selected_target, " in any of the stores in _targets.yaml")
+}
+
+parse_targets_yaml <- function() {
+  yaml::read_yaml("../interactive_location_analytics/_targets.yaml") |>
+  lapply(\(x) data.frame(script = x$script, store = x$store)) |>
+  do.call(what = rbind)
+}
+
+cat_command <- function(command) cat(format(command), "\n", sep = "")
