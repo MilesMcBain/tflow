aadf25bb6e1c2e6867a523052cf988e4a7f14ba4
Miles McBain <miles@thedatacollective.com.au>
2023-05-11 09:45:43 +1000

    use fs::path_real to get canonical paths for yaml entry comparison

--- R/helpers.R
+++ R/helpers.R
@@ -18,10 +18,10 @@ contains_quarto <- function(filepath) {
 
 current_plan_yaml_entry <- function() {
   yaml_file <- parse_targets_yaml()
-  current_file <- fs::path_file(rstudioapi::getActiveDocumentContext()$path)
+  current_file <- rstudioapi::getActiveDocumentContext()$path
 
   yaml_entry <-
-    yaml_file[yaml_file$script == current_file, ]
+    yaml_file[fs::path_real(yaml_file$script) == fs::path_real(current_file), ]
 
   if (nrow(yaml_entry) == 0) stop("{tflow} could't find an entry for current active source file in _targets.yaml")
   if (nrow(yaml_entry) > 1) stop("{tflow} found more than one entry in _targets.yaml matching the current active source file")
